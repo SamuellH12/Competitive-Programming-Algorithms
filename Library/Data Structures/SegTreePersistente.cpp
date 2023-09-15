9d36c22aa47f39bd33527ce7e3742a8a198b026f
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2023-09-15 19:18:24 -0300

    Persistent Seg Tree

--- Library/Data Structures/SegTreePersistente.cpp
+++ Library/Data Structures/SegTreePersistente.cpp
@@ -1,12 +1,4 @@
 #include <bits/stdc++.h>
-#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define ALL(x) x.begin(), x.end()
-#define endl "\n"
-#define ll long long
-#define vi vector<int>
-#define pii pair<int,int>
-#define INF 0x3f3f3f3f
-const int MAXN = 1e6 + 5;
 using namespace std;
 
 
