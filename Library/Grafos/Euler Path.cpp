8c6e5d8f65e00298491aa11bafcb4e27de34686b
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2024-09-04 12:12:50 -0300

    Add Euler Path, agora testado

--- Library/Grafos/Euler Path.cpp
+++ Library/Grafos/Euler Path.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
-#define pii pair<int, int>
 using namespace std;
+#define pii pair<int, int>
+#define vi vector<int>
 
 const int MAXN = 1e6 + 5;
 const bool BIDIRECIONAL = true;
 
 vector<pii> grafo[MAXN];
 vector<bool> used;
+int selfLoop [MAXN];
 
 void addEdge(int u, int v){
-	grafo[u].emplace_back(v, used.size());
-	if(BIDIRECIONAL && u != v) 
-		grafo[v].emplace_back(u, used.size());	
-
+	if(u == v) selfLoop[u]++;
+	grafo[u].emplace_back(v, used.size()); if(BIDIRECIONAL && u != v) 
+	grafo[v].emplace_back(u, used.size());	
 	used.emplace_back(false);
 }
 
-// !!! verifique se o grafo é conexo !!!
-vector<int> EulerPath(int n, int src=0){
+pair<vi, vi> EulerPath(int n, int src=0){
 	int s=-1, t=-1;
 
 	if(BIDIRECIONAL)
 	{
-		for(int u=0; u<n; u++)	
-			if(grafo[u].size()%2)
-				if(t != -1) return vector<int>();    // mais que 2 com grau ímpar
+		for(int u=0; u<n; u++)
+			if((grafo[u].size() - selfLoop[u])%2)
+				if(t != -1) return {vi(), vi()};   // mais que 2 com grau ímpar
 				else t = s, s = u;
-
-		if(t == -1 && t != s) return vector<int>(); // só 1 com grau ímpar
-		if(s == -1 || t == src) s = src;            // se possivel, seta start como src
+		
+		if(t == -1 && t != s) return {vi(), vi()}; // só 1 com grau ímpar
+		if(s == -1 || t == src) s = src;           // se possivel, seta start como src
 	}
 	else
 	{
@@ -38,21 +38,21 @@ vector<int> EulerPath(int n, int src=0){
 			for(auto [v, edg] : grafo[u]) 
 				in[v]++, out[u]++;
 
-		for(int u=0; u<n; u++){
-			if(in[u] == out[u]) continue;
+		for(int u=0; u<n; u++)
 			if(in[u] - out[u] == -1 && s == -1) s = u;  else
 			if(in[u] - out[u] ==  1 && t == -1) t = u;  else 
-			return vector<int>();
-		}
+			if(in[u] !=out[u]) return {vi(), vi()};
 	
-		if(s == -1 && t == -1) s = t = src;           // se possivel, seta s como src
-		if(s == -1  && t != -1) return vector<int>(); // Existe S mas não T 
-		if(s != -1  && t == -1) return vector<int>(); // Existe T mas não S
+		if(s == -1 && t == -1) s = t = src;         // se possivel, seta s como src
+		if(s == -1 && t != -1) return {vi(), vi()}; // Existe S mas não T 
+		if(s != -1 && t == -1) return {vi(), vi()}; // Existe T mas não S
 	}
+
+	for(int i=0; grafo[s].empty() && i<n; i++) s =(s+1)%n; //evita s ser vértice isolado
 
 	/////// DFS ///////
 	vector<int> path, pathId, idx(n, 0);
-	stack<pii> st; // Vertex, EdgeId
+	stack<pii> st; // {Vertex, EdgeId}
 	st.push({s, -1});
 
 	while(!st.empty())
@@ -62,7 +62,7 @@ vector<int> EulerPath(int n, int src=0){
 
 		if(idx[u] < grafo[u].size())
 		{
-			auto [v, id] = grafo[u].back();
+			auto [v, id] = grafo[u][idx[u]];
 			used[id] = true;
 			st.push({v, id});
 			continue;
@@ -74,41 +74,39 @@ vector<int> EulerPath(int n, int src=0){
 	}
 
 	pathId.pop_back();
-
 	reverse(begin(path),   end(path));
 	reverse(begin(pathId), end(pathId));
+	
+	///  Grafo conexo ? ///
+	int edgesTotal = 0;
+	for(int u=0; u<n; u++) edgesTotal += grafo[u].size() + (BIDIRECIONAL ? selfLoop[u] : 0);
+	if(BIDIRECIONAL) edgesTotal /= 2;
+	if(pathId.size() != edgesTotal) return {vi(), vi()}; 
+	//////////////////////
 
-	// return pathId; // Edges ID list
-	return path;	  // Vertex List
+	return {path, pathId};
 }
 /**************************************
-	Euler Path
-Algoritmo de Hierholzer para encontrar caminho Euleriano (Euler Path) em um grafo
-
-CUIDADO! Com grafos Desconexos. Verificar se SORCE é válido!!!
-CUIDADO! Com Self Loops
-IMPORTANTE! O algoritmo está 0-indexado
+Euler Path - Algoritmo de Hierholzer para caminho Euleriano
 
 Complexity: O(V + E)
 
+IMPORTANTE! O algoritmo está 0-indexado
+IMPORTANTE! Se fizer grafo[u].clear() defina selfLoops[u] = 0;
+
 * Informações
 	addEdge(u, v) -> Adiciona uma aresta de U para V
-	EulerPath(n, s) -> Retorna o Euler Path partindo de S, ou um vetor vazio se impossível
-	vi path -> Lista de vértices do Euler Path na ordem REVERSA a que são visitados
-	vi pathId -> id das Arestas do Euler Path na ordem REVERSA a que são visitadas
+	EulerPath(n) -> Retorna o Euler Path, ou um vetor vazio se impossível
+	selfLoop[u] -> Quantos self Loops U possui
+	vi path -> Lista de vértices do Euler Path na ordem
+	vi pathId -> id das Arestas do Euler Path na ordem
+
+Euler em Undirected graph:
+	- Cada vértice tem um número par de arestas (circuito); OU
+	- Exatamente dois vértices têm um número ímpar de arestas (caminho);
+Euler em Directed graph:
+	- Cada vértice tem quantidade de arestas |entrada| == |saída| (circuito); OU
+	- Exatamente 1 tem |entrada|+1 == |saída| && exatamente 1 tem |entrada| == |saída|+1 (caminho);
+* Circuito -> U é o primeiro e último 
+* Caminho -> U é o primeiro e V o último
 **************************************/
-/**************************************
-Para saber se um grafo possui um Caminho Euleriano:
-
-Undirected graph:
-	- Cada vértice deve ter um número par de arestas (circuito); OU
-	- Exatamente dois vértices devem ter um número ímpar de arestas (caminho);
-
-Directed graph:
-	- Cada vértice deve ter a mesma quantidade de arestas de entrada e de saída (circuito); OU
-	- Exatamente um vértice deve ter uma aresta de entrada a mais e exatemente um vértice deve ter uma aresta de saída a mais (caminho);
-
- * Circuito -> Sai do vértice U e retorna ao mesmo vértice no final
- * Caminho -> Sai de um vértice U e chega em um vértice V no final
-**************************************/
-int main(){}
