fdd311c2b2da3bfd3c268537406c4699b6f824d6
paulohltc <55456082+paulohltc@users.noreply.github.com>
2025-08-08 15:17:07 -0300

    Update Dinic.cpp
    encurtar código dinic

--- Library/Grafos/Dinic.cpp
+++ Library/Grafos/Dinic.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 #define ll long long
 
-struct Aresta {
+struct Edge {
 	int u, v; ll cap;
-	Aresta(int u, int v, ll cap) : u(u), v(v), cap(cap) {}
+	Edge(int u, int v, ll cap) : u(u), v(v), cap(cap) {}
 };
 
 struct Dinic {
 
-	int n, source, sink;
+	int n, src, sink;
 	vector<vector<int>> adj;
-	vector<Aresta> arestas;
-	vector<int> level, ptr; //pointer para a próxima aresta não saturada de cada vértice
+	vector<Edge> edges;
+	vector<int> lvl, ptr; //pointer para a próxima Edge não saturada de cada vértice
 
-	Dinic(int n, int source, int sink) : n(n), source(source), sink(sink) { adj.resize(n); }
+	Dinic(int n, int src, int sink) : n(n), src(src), sink(sink) { adj.resize(n); }
 
-	void addAresta(int u, int v, ll cap)
+	void addEdge(int u, int v, ll cap)
 	{
-		adj[u].push_back(arestas.size());
-		arestas.emplace_back(u, v, cap);
+		adj[u].push_back(edges.size());
+		edges.emplace_back(u, v, cap);
 
-		adj[v].push_back(arestas.size());
-		arestas.emplace_back(v, u, 0);
+		adj[v].push_back(edges.size());
+		edges.emplace_back(v, u, 0);
 	}
 
 	ll dfs(int u, ll flow = 1e9){
@@ -31,15 +31,15 @@ struct Dinic {
 
 		for(int &i = ptr[u];  i < adj[u].size();  i++)
 		{
-			int atual = adj[u][i];
-			int v = arestas[atual].v;
+			int at = adj[u][i];
+			int v = edges[at].v;
 
-			if(level[u] + 1 != level[v]) continue;
+			if(lvl[u] + 1 != lvl[v]) continue;
 
-			if(ll got = dfs(v, min(flow, arestas[atual].cap)) )
+			if(ll got = dfs(v, min(flow, edges[at].cap)) )
 			{
-				arestas[atual].cap -= got;
-				arestas[atual^1].cap += got;
+				edges[at].cap -= got;
+				edges[at^1].cap += got;
 				return got;
 			}
 		}
@@ -48,31 +48,31 @@ struct Dinic {
 	}
 
 	bool bfs(){
-		level = vector<int> (n, n);
-		level[source] = 0;
+		lvl = vector<int> (n, n);
+		lvl[src] = 0;
 
-		queue<int> fila;
-		fila.push(source);
+		queue<int> q;
+		q.push(src);
 
-		while(!fila.empty())
+		while(!q.empty())
 		{
-			int u = fila.front();
-			fila.pop();
+			int u = q.front();
+			q.pop();
 
 			for(auto i : adj[u]){
-				int v = arestas[i].v;
+				int v = edges[i].v;
 
-				if(arestas[i].cap == 0 || level[v] <= level[u] + 1 ) continue;
+				if(edges[i].cap == 0 || lvl[v] <= lvl[u] + 1 ) continue;
 
-				level[v] = level[u] + 1;
-				fila.push(v);
+				lvl[v] = lvl[u] + 1;
+				q.push(v);
 			}
 		}
 
-		return level[sink] < n;
+		return lvl[sink] < n;
 	}
 
-	bool inCut(int u){ return level[u] < n; }
+	bool inCut(int u){ return lvl[u] < n; }
 
 	ll maxFlow(){
 		ll ans = 0;
@@ -80,7 +80,7 @@ struct Dinic {
 		while( bfs() ){
 			ptr = vector<int> (n+1, 0);
 
-			while(ll got = dfs(source)) ans += got;
+			while(ll got = dfs(src)) ans += got;
 		}
 
 		return ans;
@@ -94,19 +94,19 @@ IMPORTANTE! O algoritmo está 0-indexado
 
 **Complexity:**
 O( V^2 * E )      ->  caso geral
-O( sqrt(V) * E )  ->  grafos com cap = 1 para toda aresta // matching bipartido
+O( sqrt(V) * E )  ->  grafos com cap = 1 para toda Edge // matching bipartido
 
 * Informações:
-	Crie o Dinic: Dinic dinic(n, source, sink);
-	Adicione as Arestas: dinic.addAresta(u, v, capacity);
+	Crie o Dinic: Dinic dinic(n, src, sink);
+	Adicione as edges: dinic.addEdge(u, v, capacity);
 	Para calcular o Fluxo Máximo: dinic.maxFlow()
 	Para saber se um vértice U está no Corte Mínimo: dinic.inCut(u)
 
 * Sobre o Código:
-	vector<Aresta> arestas;  -> Guarda todas as arestas do grafo e do grafo residual
-	vector<vector<int>> adj; -> Guarda em adj[u] os índices de todas as arestas saindo de u
-	vector<int> ptr;   ->  Pointer para a próxima aresta ainda não visitada de cada vértice
-	vector<int> level; -> Distância em vértices a partir do Source. Se igual a N o vértice não foi visitado.
+	vector<Edge> edges;  -> Guarda todas as edges do grafo e do grafo residual
+	vector<vector<int>> adj; -> Guarda em adj[u] os índices de todas as edges saindo de u
+	vector<int> ptr;   ->  Pointer para a próxima Edge ainda não visitada de cada vértice
+	vector<int> lvl; -> Distância em vértices a partir do Source. Se igual a N o vértice não foi visitado.
 	A BFS retorna se Sink é alcançavel de Source. Se não é porque foi atingido o Fluxo Máximo
 	A DFS retorna um possível aumento do Fluxo
 *****************************LATEX_DESC_END*/
@@ -114,7 +114,7 @@ O( sqrt(V) * E )  ->  grafos com cap = 1 para toda aresta // matching bipartido
 * Use Cases of Flow
 	   
 + Minimum cut: the minimum cut is equal to maximum flow.
-  i.e. to split the graph in two parts, one on the source side and another on sink side.
+  i.e. to split the graph in two parts, one on the src side and another on sink side.
   The capacity of each edge is it weight.
 
 + Edge-disjoint paths: maximum number of edge-disjoint paths equals maximum flow of the
@@ -124,8 +124,8 @@ O( sqrt(V) * E )  ->  grafos com cap = 1 para toda aresta // matching bipartido
   path, so limit the flow through a node dividing each node in two. One with incoming edges,
   other with outgoing edges and a new edge from the first to the second with capacity 1.
 
-+ Maximum matching (bipartite): maximum matching is equal to maximum flow. Add a source and
-  a sink, edges from the source to every node at one partition and from each node of the 
++ Maximum matching (bipartite): maximum matching is equal to maximum flow. Add a src and
+  a sink, edges from the src to every node at one partition and from each node of the 
   other partition to the sink.
 
 + Minimum node cover (bipartite): minimum set of nodes such each edge has at least one 
