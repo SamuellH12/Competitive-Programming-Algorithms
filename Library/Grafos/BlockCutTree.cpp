829d3e244ffa1d1bf01c16b6514eb12398f1beb6
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2025-08-10 13:27:02 -0300

    Block Cut Tree update

--- Library/Grafos/BlockCutTree.cpp
+++ Library/Grafos/BlockCutTree.cpp
@@ -2,131 +2,83 @@
 using namespace std;
 #define pii pair<int,int>
 
-const int MAXN = 1e6 + 5;
-const int MAXM = 1e6 + 5;//Cuidado
+struct BlockCutTree {
+    vector<vector<int>> g, tree, comp;
+    vector<int> id, cut;
+    BlockCutTree(int n) : n(n), g(n), cut(n) {}
 
-vector<pii> grafo [MAXN];
-int pre[MAXN], low[MAXN], clk=0, C=0;
+    void addEdge(int u, int v){
+        g[u].emplace_back(v);
+        g[v].emplace_back(u);
+    }
 
-vector<pii> edge;
-bool visEdge[MAXM];
-int edgeComponent[MAXM];
-int vertexComponent[MAXN];
+    void build(){ 
+        pre = low = id = vector<int>(n, -1);
+        for(int u=0; u<n; u++, chd=0) if(pre[u] == -1) //if graph is disconected
+            tarjan(u, -1), makeComp(-1);               //find cut vertex and make components
+        
+        for(int i=0, ct=1; i<comp.size(); i++, ct=1){  //remove components with only cuts
+            for(auto u : comp[i]) ct &= cut[u];
+            if(ct) swap(comp[i], comp.back()), comp.pop_back(), i--;
+        }
 
-int cut[MAXN];
-stack<int> s;
+        for(int u=0; u<n; u++) if(cut[u]) comp.emplace_back(1, u); //create cut components
+        
+        for(int i=0; i<comp.size(); i++) //mark id of each node
+            for(auto u : comp[i])
+                id[u] = i;
+        
+        tree.resize(comp.size()); //creates tree (looking only edges from cut points)
+        pre.assign(comp.size(), 1);
 
-vector<int> tree [2*MAXN];
-int componentSize[2*MAXN]; //vertex - cutPoints
+        for(int u=0; u<n; u++) if(cut[u]){
+            for(auto v : g[u]) if(pre[id[v]] && (!cut[v] || u<v)) addt(id[u], id[v]), pre[id[v]]=0;
+            for(auto v : g[u]) pre[id[v]] = 1;
+        }
+    }
+private:
+    vector<int> pre, low;
+    vector<pii> st;
+    int n, clk = 0, chd=0, ct, a, b;
+    void addt(int u, int v){
+        tree[u].push_back(v);
+        tree[v].push_back(u);
+    }
 
-void reset(int n){
-	for(int i=0; i<=edge.size(); i++)
-		visEdge[i] = edgeComponent[i] = 0;
+    void makeComp(int u){
+        comp.emplace_back();
+        do {
+            tie(a, b) = st.back(); 
+            st.pop_back();
+            comp.back().push_back(b);
+        } while(a != u);
+        if(~u) comp.back().push_back(u);
+    }
 
-	edge.clear();
-	
-	for(int i=0; i<=n; i++){
-		pre[i] = low[i] = -1;
-		cut[i] = false;
-		vertexComponent[i] = 0;
-		grafo[i].clear();
-	}
+    void tarjan(int u, int p){
+        pre[u] = low[u] = clk++;
+        st.emplace_back(p, u);
 
-	for(int i=0; i<=C; i++){
-		componentSize[i] = 0;
-		tree[i].clear();
-	}
-
-	while(!s.empty()) s.pop();
-
-	clk = C = 0;
-}
-
-void newComponent(int i){
-	C++;
-	int j;
-
-	do {
-		j = s.top(); s.pop();		
-		edgeComponent[j] = C;
-
-		auto [u, v]	 = edge[j];
-		if(!cut[u] && !vertexComponent[u]) componentSize[C]++, vertexComponent[u] = C;
-		if(!cut[v] && !vertexComponent[v]) componentSize[C]++, vertexComponent[v] = C;
-		
-	} while(!s.empty() && j != i);
-}
-
-void tarjan(int u, bool root = true){
-	pre[u] = low[u] = clk++;
-	
-	bool any = false;
-	int chd = 0;
-
-	for(auto [v, i] : grafo[u]){
-		if(visEdge[i]) continue;
-		visEdge[i] = true;
-
-		s.emplace(i);
-
-		if(pre[v] == -1)
-		{
-			tarjan(v, false);
-
-			low[u] = min(low[v], low[u]);
-			chd++;
-
-			if(!root && low[v] >= pre[u]) cut[u] = true, newComponent(i);
-			if( root && chd >= 2) 		  cut[u] = true, newComponent(i);
-		}
-		else
-			low[u] = min(low[u], pre[v]);
-	}
-
-	if(root) newComponent(-1);
-}
-
-//ATENCAO: ESTA 1-INDEXADO
-void buildBCC(int n){
-	vector<bool> marc(C+1, false);
-
-	for(int u=1; u<=n; u++)
-	{
-		if(!cut[u]) continue;
-
-		C++;
-		cut[u] = C;
-
-		for(auto [v, i] : grafo[u])
-		{
-			int ec = edgeComponent[i];
-			if(!marc[ec])
-			{
-				marc[ec] = true;
-				tree[cut[u]].emplace_back(ec);
-				tree[ec].emplace_back(cut[u]);
-			}
-		}
-
-		for(auto [v, i] : grafo[u]) 
-			marc[edgeComponent[i]] = false;
-	}
-}
-
-void addEdge(int u, int v){
-	int i = edge.size();
-	grafo[u].emplace_back(v, i);
-	grafo[v].emplace_back(u, i);
-	edge.emplace_back(u, v);
-}
+        for(auto v : g[u]) if(v != p){
+            if(pre[v] == -1){
+                tarjan(v, u);
+                low[u]  =  min(low[u], low[v]);
+                cut[u] |= ct = (~p && low[v] >= pre[u]) || (p==-1 && ++chd >= 2);
+                if(ct) makeComp(u);
+            }
+            else low[u] = min(low[u], pre[v]);
+        }
+    }
+};
 
 /*LATEX_DESC_BEGIN***************************
 Block Cut Tree - BiConnected Component
+BlockCutTree bcc(n);
+bcc.addEdge(u, v);
+bcc.build();
 
-reset(n);
-addEdge(u, v);
-tarjan(Root);
-buildBCC(n);
-
-No fim o grafo da Block Cut Tree estara em _vector<int> tree []_
+bcc.tree    -> graph of BlockCutTree
+bcc.id[u]   -> componet of u in the tree
+bcc.cut[u]  -> 1 if u is a cut vertex; 0 otherwise
+bcc.comp[i] -> vertex of comp i (cut are part of multiple comp)
 *****************************LATEX_DESC_END*/
