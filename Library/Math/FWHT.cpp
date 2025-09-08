07f9c36e1849341451a673f7579d776eff109994
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2025-09-08 01:36:36 -0300

    add FWHT

---
+++ Library/Math/FWHT.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int mod = 1e9+7;
+
+template<const char op> 
+vector<ll> FWHT(vector<ll> a, const bool inv = false){
+    int n = a.size();
+    for(int len=1; len<n; len+=len){
+        for(int i=0; i<n; i += 2*len){
+            for(int j=0; j<len; j++){
+                ll u = a[i+j], v = a[i+j+len];
+                
+                if(op == '^'){
+                    a[i+j] = (u+v) % mod;
+                    a[i+j+len] = (u - v+mod) % mod;
+                }
+                if(op == '|'){
+                    if(!inv) a[i+j+len] = (u+v) % mod;
+                    else a[i+j+len] = (v - u+mod) % mod;
+                }
+                if(op == '&'){
+                    if(!inv) a[i+j] = (u+v) % mod;
+                    else a[i+j] = (u - v+mod) % mod;
+                }
+            }
+        }
+    }
+
+    if(op=='^'&&inv){
+        ll rev = fexp(n, mod-2);
+        for(auto &x : a) x = x*rev % mod;
+    }
+    return a;
+}
+ 
+template<const char op> 
+vector<ll> multiply(vector<ll> a, vector<ll> b) {
+  int n = 1;
+  while(n < max(a.size(), b.size())) n <<= 1;
+
+  vector<ll> ans(n);
+  while(a.size() < ans.size()) a.push_back(0);
+  while(b.size() < ans.size()) b.push_back(0);
+  a = FWHT<op>(a);
+  b = FWHT<op>(b);
+
+  for(int i=0; i<n; i++) ans[i] = a[i]*b[i] % mod;
+  ans = FWHT<op>(ans, true);
+  return ans;
+}
+ 
+//OPTIONAL
+const int mxlog = 17;
+vector<ll> subset_multiply(vector<ll> a, vector<ll> b) {
+    int n = 1;
+    while(n < max(a.size(), b.size())) n <<= 1;
+    while(a.size() < n) a.push_back(0);
+    while(b.size() < n) b.push_back(0);
+    
+    vector<ll> ans(n);
+    vector A(mxlog+1, vector<ll>(n)), B = A;
+
+    for(int i=0; i < n; i++){
+        A[__builtin_popcount(i)][i] = a[i];
+        B[__builtin_popcount(i)][i] = b[i];
+    }
+
+    for(int i=0; i<=mxlog; i++){
+        A[i] = FWHT<'|'>(A[i]);
+        B[i] = FWHT<'|'>(B[i]);
+    }
+
+    for(int i=0; i<=mxlog; i++){
+        vector<ll> C(n);
+        
+        for (int x=0; x<=i; x++)
+            for(int j=0; j<n; j++) 
+                C[j] = (C[j] + A[x][j] * B[i-x][j] % mod) % mod;
+        
+        C = FWHT<'|'>(C, true);
+
+        for(int j=0; j < n; j++){
+        if(__builtin_popcount(j) == i)
+            ans[j] = (ans[j] + C[j]) % mod;
+    }
+
+    return ans;
+}
+
+/*LATEX_DESC_BEGIN***************************
+Fast Walsh Hadamard Transform - Convolução de XOR, OR e AND
+O(N log N)
+*****************************LATEX_DESC_END*/
+// Credits: https://github.com/gabrielpessoa1/ICPC-Library/blob/master/code/Math/FastWalshHadamardTransform.cpp
