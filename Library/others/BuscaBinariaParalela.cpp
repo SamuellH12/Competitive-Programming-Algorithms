0fcf21efc67044373dd872ce02808af6565b384f
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2025-10-06 22:01:34 -0300

    Add recursive Parallel Binary Search

--- Library/others/BuscaBinariaParalela.cpp
+++ Library/others/BuscaBinariaParalela.cpp
@@ -30,6 +30,19 @@ vector<int> pbs(int q, int k){
     return ans;
 }
 
+vector<int> ans; // recursive version
+void pbsR(vector<int> &qidx, int l, int r){
+	if(l > r) return;
+	int mid = (l+r)/2; update(mid);
+	vector<int> L, R;
+	for(auto i : qidx){
+		if(check(i)) L.push_back(i), ans[i] = mid;
+		else R.push_back(i);
+	}
+	if(!L.empty()) pbsR(L, l, mid-1);
+	if(!R.empty()) pbsR(R, mid+1, r);
+}
+
 /*LATEX_DESC_BEGIN***************************
 Busca Binária Paralela - O(Q+K log K)
 
@@ -39,4 +52,5 @@ De forma que:
 - A resposta é monotônica (se é verdadeiro para t=x, é verdade para t'=x+1)
 - É possível responder em O(N^2) iterando pelos updates e verificando a cada passo todas as queries não satisfeitas ainda.
 
+* No caso do range de busca ser [0, 1e9]: recursivo OU veja se é possível olhar apenas os valores que aparecem no input(?)
 *****************************LATEX_DESC_END*/
