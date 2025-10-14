2c9ed7d49241430d6a7e1fcbd8f09b72f895e9e9
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2025-10-14 20:02:50 -0300

    mint assign operators

--- Library/Math/mint.cpp
+++ Library/Math/mint.cpp
@@ -2,8 +2,7 @@
 #include "fexp.cpp" //LATEX_IGNORED_LINE
 #define ll long long
 using namespace std;
-
-const ll mod = 1e9+7;
+const ll mod = 1e9+7;//LATEX_IGNORED_LINE
 
 struct mint {
 	ll v = 0;
@@ -12,5 +11,13 @@ struct mint {
 	mint operator- (const mint &b) const { ll a = v-b.v; return a <  0  ? a+mod : a; }
 	mint operator* (const mint &b) const { return v * b.v % mod; }
 	mint operator/ (const mint &b) const { return v * fexp(b.v, mod-2) % mod;  }
-    bool operator< (const mint &b) const { return v < b.v; }
 };
+// Extra Operators - Bool Operators
+bool operator==(const mint&a, const mint &b){ return a.v == b.v; }
+bool operator!=(const mint&a, const mint &b){ return a.v != b.v; }
+bool operator< (const mint&a, const mint &b){ return a.v <  b.v; }
+// Assignment Operators
+mint operator+= (mint&a, const mint &b){ return a = a + b; } 
+mint operator-= (mint&a, const mint &b){ return a = a - b; }
+mint operator*= (mint&a, const mint &b){ return a = a * b; }
+mint operator/= (mint&a, const mint &b){ return a = a / b; }
